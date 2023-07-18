2fc2b633bb0e0377d0c4453f494b5bc2939c3f78
susie <susie0905@naver.com>
2023-07-18 17:49:59 +0900

    2주차 딕셔너리

---
+++ second copy 3.py
@@ -0,0 +1,158 @@
+map = [
+    {
+        "title": "동국대학교 서울캠퍼스",
+        "address": "서울 중구 필동3가 26-1",
+        "roadAddress": "서울 중구 필동로1길 30",
+        "longitude": 127.00090541880259,
+        "latitude": 37.55808646250296,
+        "x": 60,
+        "y": 127,
+        "dongCode": "1114057000",
+        "category": "",
+        "titleEn": "Donggukdaehakgyo Seoulkaempeoseu",
+        "addressEn": "Seoul Jung-gu Pildong 3-ga 26-1",
+        "roadAddressEn": "Seoul Jung-gu Pildong-ro 1-gil 30"
+    },
+    {
+        "title": "동국대학교 WISE캠퍼스",
+        "address": "경북 경주시 석장동 707",
+        "roadAddress": "경북 경주시 동대로 123",
+        "longitude": 129.19450495505598,
+        "latitude": 35.862538617367974,
+        "x": 99,
+        "y": 91,
+        "dongCode": "4713036000",
+        "category": "",
+        "titleEn": "Donggukdaehakgyo WISEKaempeoseu",
+        "addressEn": "Gyeongbuk Gyeongju-si Seokjang-dong 707",
+        "roadAddressEn": "Gyeongbuk Gyeongju-si Dong-daero 123"
+    },
+    {
+        "title": "동국대학교 서울캠퍼스 이해랑예술극장",
+        "address": "서울 중구 장충동2가 192-5",
+        "roadAddress": "서울 중구 필동로1길 30",
+        "longitude": 127.003309315842,
+        "latitude": 37.5583152728918,
+        "x": 60,
+        "y": 127,
+        "dongCode": "1114057000",
+        "category": "",
+        "titleEn": "Donggukdaehakgyo Seoulkaempeoseu Ihaerang-yesulgeukjang",
+        "addressEn": "Seoul Jung-gu Jangchungdong 2-ga 192-5",
+        "roadAddressEn": "Seoul Jung-gu Pildong-ro 1-gil 30"
+    },
+    {
+        "title": "동국대학교 혜화관",
+        "address": "서울 중구 장충동2가 192-5",
+        "roadAddress": "서울 중구 필동로1길 30",
+        "longitude": 127.001874208571,
+        "latitude": 37.5577530821104,
+        "x": 60,
+        "y": 127,
+        "dongCode": "1114057000",
+        "category": "",
+        "titleEn": "Donggukdaehakgyo Hyehwagwan",
+        "addressEn": "Seoul Jung-gu Jangchungdong 2-ga 192-5",
+        "roadAddressEn": "Seoul Jung-gu Pildong-ro 1-gil 30"
+    },
+    {
+        "title": "동국대학교 지하주차장",
+        "address": "서울 중구 필동3가 26-1",
+        "roadAddress": "서울 중구 필동로1길 30",
+        "longitude": 127.002037155905,
+        "latitude": 37.5567493683421,
+        "x": 60,
+        "y": 127,
+        "dongCode": "1114057000",
+        "category": "",
+        "titleEn": "Donggukdaehakgyo Jihajuchajang",
+        "addressEn": "Seoul Jung-gu Pildong 3-ga 26-1",
+        "roadAddressEn": "Seoul Jung-gu Pildong-ro 1-gil 30"
+    },
+    {
+        "title": "동국대학교 서울캠퍼스 후문",
+        "address": "서울 중구 필동3가 26-1",
+        "roadAddress": "서울 중구 필동로1길 30",
+        "longitude": 126.99936392096474,
+        "latitude": 37.56071287063473,
+        "x": 60,
+        "y": 127,
+        "dongCode": "1114057000",
+        "category": "",
+        "titleEn": "Donggukdaehakgyo Seoulkaempeoseu Humun",
+        "addressEn": "Seoul Jung-gu Pildong 3-ga 26-1",
+        "roadAddressEn": "Seoul Jung-gu Pildong-ro 1-gil 30"
+    },
+    {
+        "title": "동국대학교 중앙도서관",
+        "address": "서울 중구 장충동2가 192-5",
+        "roadAddress": "서울 중구 필동로1길 30",
+        "longitude": 126.999099109702,
+        "latitude": 37.557969332895,
+        "x": 60,
+        "y": 127,
+        "dongCode": "1114057000",
+        "category": "",
+        "titleEn": "Donggukdaehakgyo Jung-angdoseogwan",
+        "addressEn": "Seoul Jung-gu Jangchungdong 2-ga 192-5",
+        "roadAddressEn": "Seoul Jung-gu Pildong-ro 1-gil 30"
+    },
+    {
+        "title": "동국대학교 문화관",
+        "address": "서울 중구 장충동2가 192-5",
+        "roadAddress": "서울 중구 필동로1길 30",
+        "longitude": 127.003082941295,
+        "latitude": 37.557850364443,
+        "x": 60,
+        "y": 127,
+        "dongCode": "1114057000",
+        "category": "",
+        "titleEn": "Donggukdaehakgyo Munhwagwan",
+        "addressEn": "Seoul Jung-gu Jangchungdong 2-ga 192-5",
+        "roadAddressEn": "Seoul Jung-gu Pildong-ro 1-gil 30"
+    },
+    {
+        "title": "동국대학교 충무로영상센터 주차장",
+        "address": "서울 중구 필동2가 82-1",
+        "roadAddress": "서울 중구 퇴계로36길 2",
+        "longitude": 126.99366074472721,
+        "latitude": 37.56035140242095,
+        "x": 60,
+        "y": 127,
+        "dongCode": "1114060500",
+        "category": "",
+        "titleEn": "Donggukdaehakgyo Chungmuroyeongsangsenteo Juchajang",
+        "addressEn": "Seoul Jung-gu Pildong 2-ga 82-1",
+        "roadAddressEn": "Seoul Jung-gu Toegye-ro 36-gil 2"
+    },
+    {
+        "title": "동국대학교 신공학관",
+        "address": "서울 중구 필동3가 28-47",
+        "roadAddress": "서울 중구 필동로1길 30",
+        "longitude": 126.998311394625,
+        "latitude": 37.5580558199932,
+        "x": 60,
+        "y": 127,
+        "dongCode": "1114057000",
+        "category": "",
+        "titleEn": "Donggukdaehakgyo Sin-gonghakgwan",
+        "addressEn": "Seoul Jung-gu Pildong 3-ga 28-47",
+        "roadAddressEn": "Seoul Jung-gu Pildong-ro 1-gil 30"
+    }
+]
+
+#3. 문자열 split과 replace
+#a.split() 공백을 기준으로. b.split(':') :를 기준으로
+search=input("검색어를 입력하세요:")
+split=search.split()
+print(split)
+#반복문하고 문자열에 동국있는지 if in으로 확인 
+for a in range(len(split)):
+    if '동국' in split[a]:
+       temp=a
+del split[temp]
+print(split)
+for i in range(len(map)):
+    for j in range(len(split)):
+        if split[j] in map[i]['title']:
+            print(map[i]['address'])
---
+++ second copy.py
@@ -0,0 +1,159 @@
+map = [
+    {
+        "title": "동국대학교 서울캠퍼스",
+        "address": "서울 중구 필동3가 26-1",
+        "roadAddress": "서울 중구 필동로1길 30",
+        "longitude": 127.00090541880259,
+        "latitude": 37.55808646250296,
+        "x": 60,
+        "y": 127,
+        "dongCode": "1114057000",
+        "category": "",
+        "titleEn": "Donggukdaehakgyo Seoulkaempeoseu",
+        "addressEn": "Seoul Jung-gu Pildong 3-ga 26-1",
+        "roadAddressEn": "Seoul Jung-gu Pildong-ro 1-gil 30"
+    },
+    {
+        "title": "동국대학교 WISE캠퍼스",
+        "address": "경북 경주시 석장동 707",
+        "roadAddress": "경북 경주시 동대로 123",
+        "longitude": 129.19450495505598,
+        "latitude": 35.862538617367974,
+        "x": 99,
+        "y": 91,
+        "dongCode": "4713036000",
+        "category": "",
+        "titleEn": "Donggukdaehakgyo WISEKaempeoseu",
+        "addressEn": "Gyeongbuk Gyeongju-si Seokjang-dong 707",
+        "roadAddressEn": "Gyeongbuk Gyeongju-si Dong-daero 123"
+    },
+    {
+        "title": "동국대학교 서울캠퍼스 이해랑예술극장",
+        "address": "서울 중구 장충동2가 192-5",
+        "roadAddress": "서울 중구 필동로1길 30",
+        "longitude": 127.003309315842,
+        "latitude": 37.5583152728918,
+        "x": 60,
+        "y": 127,
+        "dongCode": "1114057000",
+        "category": "",
+        "titleEn": "Donggukdaehakgyo Seoulkaempeoseu Ihaerang-yesulgeukjang",
+        "addressEn": "Seoul Jung-gu Jangchungdong 2-ga 192-5",
+        "roadAddressEn": "Seoul Jung-gu Pildong-ro 1-gil 30"
+    },
+    {
+        "title": "동국대학교 혜화관",
+        "address": "서울 중구 장충동2가 192-5",
+        "roadAddress": "서울 중구 필동로1길 30",
+        "longitude": 127.001874208571,
+        "latitude": 37.5577530821104,
+        "x": 60,
+        "y": 127,
+        "dongCode": "1114057000",
+        "category": "",
+        "titleEn": "Donggukdaehakgyo Hyehwagwan",
+        "addressEn": "Seoul Jung-gu Jangchungdong 2-ga 192-5",
+        "roadAddressEn": "Seoul Jung-gu Pildong-ro 1-gil 30"
+    },
+    {
+        "title": "동국대학교 지하주차장",
+        "address": "서울 중구 필동3가 26-1",
+        "roadAddress": "서울 중구 필동로1길 30",
+        "longitude": 127.002037155905,
+        "latitude": 37.5567493683421,
+        "x": 60,
+        "y": 127,
+        "dongCode": "1114057000",
+        "category": "",
+        "titleEn": "Donggukdaehakgyo Jihajuchajang",
+        "addressEn": "Seoul Jung-gu Pildong 3-ga 26-1",
+        "roadAddressEn": "Seoul Jung-gu Pildong-ro 1-gil 30"
+    },
+    {
+        "title": "동국대학교 서울캠퍼스 후문",
+        "address": "서울 중구 필동3가 26-1",
+        "roadAddress": "서울 중구 필동로1길 30",
+        "longitude": 126.99936392096474,
+        "latitude": 37.56071287063473,
+        "x": 60,
+        "y": 127,
+        "dongCode": "1114057000",
+        "category": "",
+        "titleEn": "Donggukdaehakgyo Seoulkaempeoseu Humun",
+        "addressEn": "Seoul Jung-gu Pildong 3-ga 26-1",
+        "roadAddressEn": "Seoul Jung-gu Pildong-ro 1-gil 30"
+    },
+    {
+        "title": "동국대학교 중앙도서관",
+        "address": "서울 중구 장충동2가 192-5",
+        "roadAddress": "서울 중구 필동로1길 30",
+        "longitude": 126.999099109702,
+        "latitude": 37.557969332895,
+        "x": 60,
+        "y": 127,
+        "dongCode": "1114057000",
+        "category": "",
+        "titleEn": "Donggukdaehakgyo Jung-angdoseogwan",
+        "addressEn": "Seoul Jung-gu Jangchungdong 2-ga 192-5",
+        "roadAddressEn": "Seoul Jung-gu Pildong-ro 1-gil 30"
+    },
+    {
+        "title": "동국대학교 문화관",
+        "address": "서울 중구 장충동2가 192-5",
+        "roadAddress": "서울 중구 필동로1길 30",
+        "longitude": 127.003082941295,
+        "latitude": 37.557850364443,
+        "x": 60,
+        "y": 127,
+        "dongCode": "1114057000",
+        "category": "",
+        "titleEn": "Donggukdaehakgyo Munhwagwan",
+        "addressEn": "Seoul Jung-gu Jangchungdong 2-ga 192-5",
+        "roadAddressEn": "Seoul Jung-gu Pildong-ro 1-gil 30"
+    },
+    {
+        "title": "동국대학교 충무로영상센터 주차장",
+        "address": "서울 중구 필동2가 82-1",
+        "roadAddress": "서울 중구 퇴계로36길 2",
+        "longitude": 126.99366074472721,
+        "latitude": 37.56035140242095,
+        "x": 60,
+        "y": 127,
+        "dongCode": "1114060500",
+        "category": "",
+        "titleEn": "Donggukdaehakgyo Chungmuroyeongsangsenteo Juchajang",
+        "addressEn": "Seoul Jung-gu Pildong 2-ga 82-1",
+        "roadAddressEn": "Seoul Jung-gu Toegye-ro 36-gil 2"
+    },
+    {
+        "title": "동국대학교 신공학관",
+        "address": "서울 중구 필동3가 28-47",
+        "roadAddress": "서울 중구 필동로1길 30",
+        "longitude": 126.998311394625,
+        "latitude": 37.5580558199932,
+        "x": 60,
+        "y": 127,
+        "dongCode": "1114057000",
+        "category": "",
+        "titleEn": "Donggukdaehakgyo Sin-gonghakgwan",
+        "addressEn": "Seoul Jung-gu Pildong 3-ga 28-47",
+        "roadAddressEn": "Seoul Jung-gu Pildong-ro 1-gil 30"
+    }
+]
+
+#1. 위 변수와 반복문, 조건문을 이용하여 “동국대학교 신공학관”의 도로명주소, 위도, 경도를 출력하여 봅시다! (Hint : 딕셔너리, if-in)
+for i in range(len(map)):
+    if '동국대학교 신공학관' in map[i]['title']:
+        print(map[i]['address'])
+        print(map[i]['x'])
+        print(map[i]['y'])
+
+#2. 특정 문자를 입력 받아(input 함수 이용) “address”키 내용으로 해당 문자가 있는 모든 딕셔너리의 “title”값를 출력하세요.
+
+search=input()
+for i in range(len(map)):
+    if search in map[i]['address']:
+        print(map[i]['title'])
+
+
+
